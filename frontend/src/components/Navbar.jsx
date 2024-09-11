8e05d3907352fbc9ac0f0b7f96607f87501ef6b6
Tuukka Salonen <tuukka.t.salonen@tuni.fi>
2024-09-11 23:40:44 +0300

    style: Minor styling adjustments + set website icon

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styling/Navbar.css';
+import Button from '@mui/material/Button';
 
 export const Navbar = () => {
 	return (
 		<nav className="navbar">
 			<Link to="/" className="navbar-link">
-				Home
+				<Button variant="contained" style={{ borderRadius: 30 }}>
+					Home
+				</Button>
 			</Link>
 			<Link to="/details" className="navbar-link">
-				Personal details
+				<Button variant="contained" style={{ borderRadius: 30 }}>
+					CV
+				</Button>
 			</Link>
 			<Link to="/projects" className="navbar-link">
-				Projects
+				<Button variant="contained" style={{ borderRadius: 30 }}>
+					Projects
+				</Button>
 			</Link>
 		</nav>
 	);
--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -1,9 +1,29 @@
 import './styling/Home.css';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export const NotFound = () => {
+	const navigate = useNavigate();
+	const [seconds, setSeconds] = useState(5);
+
+	useEffect(() => {
+		if (seconds > 0) {
+			const timer = setTimeout(() => {
+				setSeconds((prev) => prev - 1);
+			}, 1000);
+
+			return () => clearTimeout(timer);
+		}
+		else {
+			navigate('/');
+		}
+	}, [seconds, navigate]);
+
 	return (
 		<div className="home-container">
-			<div className="home-container-text">Page not found</div>
+			<div className="home-container-text">OOPS!</div>
+			<div>This page was not found.</div>
+			<div>Redirecting back to home page in {seconds} seconds.</div>
 		</div>
 	);
 };
